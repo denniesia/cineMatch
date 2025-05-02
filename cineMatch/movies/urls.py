00ba4fc1c2325c40e8e7987f3a8f83c6703828e5
dennie <denitsa.sureva@icloud.com>
2025-05-02 16:11:39 +0200

    feat:landing page

--- cineMatch/movies/urls.py
+++ cineMatch/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import dashboard
+from .views import dashboard, index, movies_view
 
 urlpatterns = [
-    path('', dashboard, name='dashboard'),
+    path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('movies/', movies_view, name='movies'),
 ]
--- cineMatch/movies/views.py
+++ cineMatch/movies/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def movies_view(request):
+    TMDB_API_KEY = 'a186f459fadf51ad863d0bb6e9e85805'
+    url = f'https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=en-US&page=1'
+    data = requests.get(url).json()
+    movies = data.get('results', [])
+    return render(request, 'overview.html', {'movies': movies})
