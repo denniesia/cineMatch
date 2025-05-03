c67d54e6547152686d3ab5e6991454c7ef1d527b
dennie <denitsa.sureva@icloud.com>
2025-05-03 11:25:45 +0200

    feat:landing page with categories

--- cineMatch/movies/urls.py
+++ cineMatch/movies/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from .views import dashboard, index, movies_view
+from .views import index, movies_view
 
 urlpatterns = [
     path('', index, name='index'),
-    path('dashboard/', dashboard, name='dashboard'),
     path('movies/', movies_view, name='movies'),
 ]
--- cineMatch/movies/views.py
+++ cineMatch/movies/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import requests
 # Create your views here.
+TMDB_API_KEY = 'a186f459fadf51ad863d0bb6e9e85805'
+
 def index(request):
-    return render(request, 'index.html')
-def dashboard(request):
-    return render(request, 'dashboard.html')
+    url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={TMDB_API_KEY}&language=en-US"
+    data = requests.get(url).json()
+    movie_genres = data.get('genres', [])
+    genre_names = [genre["name"] for genre in movie_genres]
+    context = {'genre_names': genre_names}
+    return render(request, 'index.html', context)
+
 
 def movies_view(request):
     TMDB_API_KEY = 'a186f459fadf51ad863d0bb6e9e85805'
